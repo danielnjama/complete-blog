911a5d1d39418ffb9b7e89440f63dc5efe96be7b
Daniel Wangari <danielnjama2015@gmail.com>
2023-10-30 09:58:45 +0300

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -37,9 +37,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'myblog.urls'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
