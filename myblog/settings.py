d9d109a4e8a8b982db3dadf6e17e575fbbd2ac72
Daniel Wangari <danielnjama2015@gmail.com>
2023-10-30 10:50:23 +0300

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import dj-database-url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
